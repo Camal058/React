d72e2424271a5d60f918159f09f8cb3458ae8ccb
Camal <87723982+Camal058@users.noreply.github.com>
2024-02-29 23:59:44 +0400

    Add files via upload

---
+++ ProjectWork/src/Header/Header.jsx
@@ -0,0 +1,17 @@
+import './Header.css';
+import { Link } from "react-router-dom";
+
+export function Header() {
+    return (
+        <header className="header-container">
+            <nav className="nav-container">
+                <h1 className="nav-logo">Techno</h1>
+                
+                <ul className="nav-list">
+                    <li className="nav-list-item"><Link to='/'><span>Catalog</span></Link></li>
+                    <li className="nav-list-item"><Link to='/myBag'><span>My bag</span></Link></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
