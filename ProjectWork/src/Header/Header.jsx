3114e3b0e58d66b445141a719dc20a677fdb26eb
Camal <87723982+Camal058@users.noreply.github.com>
2024-03-01 11:06:42 +0400

    Update Header.jsx

--- ProjectWork/src/Header/Header.jsx
+++ ProjectWork/src/Header/Header.jsx
@@ -6,7 +6,6 @@ export function Header() {
         <header className="header-container">
             <nav className="nav-container">
                 <h1 className="nav-logo">Techno</h1>
-                
                 <ul className="nav-list">
                     <li className="nav-list-item"><Link to='/'><span>Catalog</span></Link></li>
                     <li className="nav-list-item"><Link to='/myBag'><span>My bag</span></Link></li>
